86e01fcd7c0fc93e26ab1f6624c6564dfb7842c5
Mabarik667f <111702128+Mabarik667f@users.noreply.github.com>
2023-11-10 01:10:14 +0300

    updated base_buttons func and added minutes keyboards, fix create_time and create_move_button

--- keyboards/kb_utils.py
+++ keyboards/kb_utils.py
@@ -4,9 +4,19 @@ from aiogram.utils.keyboard import ReplyKeyboardMarkup, ReplyKeyboardBuilder, \
 from lexicon.lexicon import LEXICON, LEXICON_BUTTONS
 
 
-def add_base_buttons(buttons: list[InlineKeyboardButton]) -> None:
-    buttons.append(InlineKeyboardButton(text=LEXICON['back'],
-                                        callback_data='back'))
+def add_base_buttons(keyboard: InlineKeyboardBuilder, *args) -> None:
+    buttons: list[InlineKeyboardButton] = [InlineKeyboardButton(text=LEXICON_BUTTONS['menu'],
+                                                                callback_data='menu'),
+                                           InlineKeyboardButton(text=LEXICON['back'],
+                                                                callback_data='back')]
+    if args:
+        if args[0]:
+            if args[0].text == '<':
+                buttons.insert(0, args[0])
+        if args[1]:
+            if args[1].text == '>':
+                buttons.insert(len(buttons), args[1])
+    keyboard.row(*buttons, width=4)
 
 
 # Клавиатура для кнопок создать и удалить напоминание
@@ -31,38 +41,46 @@ notification_add: list[InlineKeyboardButton] = [InlineKeyboardButton(text=LEXICO
                                                                      callback_data=key)
                                                 for key in LEXICON_BUTTONS.keys()
                                                 if key.endswith('_add')]
-add_base_buttons(notification_add)
+
 notification_add_builder: InlineKeyboardBuilder = InlineKeyboardBuilder()
 notification_add_builder.row(*notification_add, width=3)
+add_base_buttons(notification_add_builder)
 notification_add_kb = notification_add_builder.as_markup()
 
 
 # клавиатура часов
 
-def create_move_button(buttons, forward: bool = False, backward: bool = False) -> None:
+def create_move_button(forward: bool = False, backward: bool = False) -> tuple:
+    backward_button, forward_button = False, False
     if backward:
-        buttons.append(InlineKeyboardButton(text=LEXICON_BUTTONS['backward'],
-                                            callback_data=LEXICON_BUTTONS['backward']))
+        backward_button = (InlineKeyboardButton(text=LEXICON_BUTTONS['backward'],
+                                                callback_data=LEXICON_BUTTONS['backward']))
     if forward:
-        buttons.append(InlineKeyboardButton(text=LEXICON_BUTTONS['forward'],
-                                            callback_data=LEXICON_BUTTONS['forward']))
+        forward_button = (InlineKeyboardButton(text=LEXICON_BUTTONS['forward'],
+                                               callback_data=LEXICON_BUTTONS['forward']))
+
+    return backward_button, forward_button
 
 
 def create_time_keyboard(j: int, k: int, time_type: str, forward: bool = False,
-                         backward: bool = False) -> InlineKeyboardMarkup:
+                         backward: bool = False, width: int = 4) -> InlineKeyboardMarkup:
     time_ls: list[InlineKeyboardButton] = []
     for i in range(j, k):
         text = i
         callback_data = f"{i}_{time_type}"
         time_ls.append(InlineKeyboardButton(text=text, callback_data=callback_data))
 
-    add_base_buttons(time_ls)
-    create_move_button(time_ls, forward, backward)
     time_ls_builder: InlineKeyboardBuilder = InlineKeyboardBuilder()
-    time_ls_builder.row(*time_ls, width=3)
+    time_ls_builder.row(*time_ls, width=width)
+    backward_button, forward_button = create_move_button(backward=backward, forward=forward)
+    add_base_buttons(time_ls_builder, backward_button, forward_button)
     time_ls_kb = time_ls_builder.as_markup()
     return time_ls_kb
 
 
-hour_choice_am = create_time_keyboard(0, 12, 'hour', forward=True)
-hour_choice_pm = create_time_keyboard(12, 24, 'hour', backward=True)
+hour_choice = create_time_keyboard(0, 24, 'hour')
+
+minutes_15 = create_time_keyboard(0, 15, 'minute', forward=True, width=5)
+minutes_30 = create_time_keyboard(15, 30, 'minute', forward=True, backward=True, width=5)
+minutes_45 = create_time_keyboard(30, 45, 'minute', forward=True, backward=True, width=5)
+minutes_60 = create_time_keyboard(45, 60, 'minute', backward=True, width=5)
