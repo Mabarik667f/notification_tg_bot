56469d41828ad98fec5f9d910e29510f7138c068
Mabarik667f <111702128+Mabarik667f@users.noreply.github.com>
2023-11-12 22:22:44 +0300

    create first iteration models

---
+++ models/models.py
@@ -0,0 +1,72 @@
+import pymysql
+from config_data.config import load_config
+
+cfg = load_config()
+
+
+def connection_db():
+    connection = pymysql.connect(database=cfg.db.database,
+                                 user=cfg.db.db_user,
+                                 password=cfg.db.db_password,
+                                 host=cfg.db.db_host,
+                                 port=3306)
+
+    return connection
+
+
+db = connection_db()
+try:
+    with db.cursor() as cursor:
+        cursor.execute('''
+        CREATE TABLE IF NOT EXISTS DayOfWeek (
+            id INT AUTO_INCREMENT PRIMARY KEY, 
+            day_name VARCHAR(2) UNIQUE
+            )
+        ''')
+
+        cursor.execute('''
+        CREATE TABLE IF NOT EXISTS User (
+        user_id INT PRIMARY KEY
+            )
+        ''')
+
+        cursor.execute('''
+        CREATE TABLE IF NOT EXISTS Notification (
+            id INT AUTO_INCREMENT PRIMARY KEY,
+            user_Id INT,
+            notification_time TIME, 
+            text VARCHAR(50),
+            FOREIGN KEY(user_id) REFERENCES User(user_id)
+            )
+        ''')
+
+        cursor.execute('''
+        CREATE TABLE IF NOT EXISTS ExactDate (
+            id INT AUTO_INCREMENT PRIMARY KEY,
+            date DATE,
+            notification_id INT,
+            FOREIGN KEY(notification_id) REFERENCES Notification(id)
+            )
+        ''')
+
+        cursor.execute('''
+        CREATE TABLE IF NOT EXISTS WeekDayHasNotification (
+            week_day_id INT,
+            notification_id INT,
+            FOREIGN KEY(notification_id) REFERENCES Notification(id),
+            FOREIGN KEY(week_day_id) REFERENCES DayOfWeek(id)
+            )
+        ''')
+
+    with db.cursor() as cursor:
+        cursor.execute('''SELECT COUNT(*) FROM DayOfWeek''')
+        count = cursor.fetchone()[0]
+        day_of_the_week = ['Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб', 'Вс']
+        if count == 0:
+            for day in day_of_the_week:
+                cursor.execute('INSERT INTO DayOfWeek (day_name) VALUES (%s)', (day,))
+
+    db.commit()
+
+finally:
+    db.close()
