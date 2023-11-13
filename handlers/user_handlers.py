9811804648a62401910f9470e3cfb1372748c0eb
Mabarik667f <111702128+Mabarik667f@users.noreply.github.com>
2023-11-13 23:26:44 +0300

    add years, months, hours, minutes, week days and new lexicon

--- handlers/user_handlers.py
+++ handlers/user_handlers.py
@@ -2,11 +2,14 @@ from aiogram import Router, F
 from aiogram.filters import CommandStart, StateFilter, Command
 from aiogram.fsm.context import FSMContext
 from aiogram.fsm.state import default_state, any_state
-from aiogram.types import Message, CallbackQuery
-from filters.filters import IsTimeCallbackData, IsHourCallbackData
+from aiogram.types import Message, CallbackQuery, InlineKeyboardMarkup
+# from filters.filters import IsTimeCallbackData, IsHourCallbackData, IsMinuteCallbackData, CheckWeekDayCallbackData
+from filters.filters import IsDateTimePostfixCallbackData
 from bot import NotificationFSM
+from services.services import change_curr_note_info
 from keyboards.kb_utils import *
 from lexicon.lexicon import LEXICON, LEXICON_BUTTONS
+from models.methods import register_user
 
 router = Router()
 
@@ -14,9 +17,12 @@ router = Router()
 # Базовые команды или кнопки
 
 
-@router.message(CommandStart(), StateFilter(default_state))
+@router.message(CommandStart(), StateFilter(any_state))
 async def handler_start(message: Message, state: FSMContext):
+    register_user(message.from_user.id)
     await message.answer(text=LEXICON['/start'], reply_markup=notification_methods_kb)
+
+    await state.set_state(NotificationFSM.menu_state)
 
 
 @router.message(Command(commands='menu'), StateFilter(any_state))
@@ -24,6 +30,8 @@ async def handler_menu(message: Message, state: FSMContext):
     await message.answer(text=LEXICON['/start'], reply_markup=notification_methods_kb)
 
     await state.set_state(NotificationFSM.menu_state)
+
+    await change_curr_note_info()
 
 
 @router.callback_query(F.data == 'menu', StateFilter(any_state))
@@ -39,30 +47,38 @@ async def handler_menu_callback(callback: CallbackQuery, state: FSMContext):
 @router.callback_query(F.data == 'add_notification', StateFilter(NotificationFSM.menu_state))
 async def handler_callback_add_notification(callback: CallbackQuery, state: FSMContext):
     await callback.message.edit_text(text=LEXICON['/add_notification'],
-                                     reply_markup=notification_add_kb)
+                                     reply_markup=hour_choice)
+
+    await state.set_state(NotificationFSM.hour_choice_state)
 
 
 @router.message(Command(commands='add_notification'), StateFilter(NotificationFSM.menu_state))
 async def handler_add_notification(message: Message, state: FSMContext):
     await message.answer(text=LEXICON['/add_notification'],
-                         reply_markup=notification_add_kb)
+                         reply_markup=hour_choice)
 
-
-@router.callback_query(IsTimeCallbackData())  # Состояние
-async def handler_add_fixed_notification(callback: CallbackQuery, state: FSMContext):
-    """Добавление напоминания через фиксированное время"""
-    await callback.message.edit_text(text=LEXICON['notification_added'],
-                                     reply_markup=notification_methods_kb)
-    print(callback.data.endswith('_add'))
-
-
-@router.callback_query(F.data.in_(['custom_add', 'day_add']))  # Состояние
-async def handler_hour_choice_notification(callback: CallbackQuery, state: FSMContext):
-    """Для добавления интервала времени для даты, большей чем день"""
     await state.set_state(NotificationFSM.hour_choice_state)
 
-    await callback.message.edit_text(text=LEXICON['custom_notification'],
-                                     reply_markup=hour_choice)
+
+@router.callback_query(IsDateTimePostfixCallbackData('_hour'), StateFilter(NotificationFSM.hour_choice_state))
+async def handler_custom_add_minutes_notification(callback: CallbackQuery, state: FSMContext):
+    text = change_curr_note_info(message=LEXICON['pick_minutes'], hour=callback.data)
+
+    await callback.message.edit_text(text=text,
+                                     reply_markup=minutes_15)
+
+    await state.set_state(NotificationFSM.minutes_15_state)
+
+
+@router.callback_query(IsDateTimePostfixCallbackData('_minute'), StateFilter(NotificationFSM.minutes_15_state,
+                                                           NotificationFSM.minutes_30_state,
+                                                           NotificationFSM.minutes_45_state,
+                                                           NotificationFSM.minutes_60_state))
+async def handler_choice_date(callback: CallbackQuery, state: FSMContext):
+
+    await callback.message.edit_text(text=LEXICON['choice_days'],
+                                     reply_markup=select_days_kb)
+    await state.set_state(NotificationFSM.choice_days_state)
 
 
 @router.callback_query(F.data == '>', StateFilter(NotificationFSM.minutes_15_state,
@@ -106,16 +122,30 @@ async def handler_backward_notification(callback: CallbackQuery, state: FSMConte
                                          reply_markup=reply_markup)
 
 
-@router.callback_query(IsHourCallbackData(), StateFilter(NotificationFSM.hour_choice_state))
-async def handler_custom_add_minutes_notification(callback: CallbackQuery, state: FSMContext):
-    await callback.message.edit_text(text=LEXICON['pick_minutes'],
-                                     reply_markup=minutes_15)
+@router.callback_query(F.data == 'week_days', StateFilter(NotificationFSM.choice_days_state))
+async def handler_choice_week_days(callback: CallbackQuery, state: FSMContext):
+    await callback.message.edit_text(text=LEXICON['choice_days'],
+                                     reply_markup=day_week_choice_kb)
+    await state.set_state(NotificationFSM.week_days_state)
 
-    await state.set_state(NotificationFSM.minutes_15_state)
+
+@router.callback_query(F.data == 'exact_date', StateFilter(NotificationFSM.choice_days_state))
+async def handler_exact_year(callback: CallbackQuery, state: FSMContext):
+    await callback.message.edit_text(text=LEXICON['choice_year'],
+                                     reply_markup=choice_year_kb)
+
+    await state.set_state(NotificationFSM.year_choice_state)
+
+
+@router.callback_query(IsDateTimePostfixCallbackData('_year'), StateFilter(NotificationFSM.year_choice_state))
+async def handler_exact_month(callback: CallbackQuery, state: FSMContext):
+    await callback.message.edit_text(text=LEXICON['month_choice'],
+                                     reply_markup=choice_month_kb)
+
+    await state.set_state(NotificationFSM.month_choice_state)
 
 
 # Раздел с обработкой кнопок и команд для удаления напоминаний
-
 
 @router.callback_query(F.data == 'remove_notification', StateFilter(NotificationFSM.menu_state))
 async def handler_callback_remove_notification(callback: CallbackQuery, state: FSMContext):
--- lexicon/lexicon.py
+++ lexicon/lexicon.py
@@ -8,7 +8,11 @@ LEXICON: dict = {
     'menu': 'Меню',
     'notification_added': '✅ Напоминание установленно',
     'custom_notification': 'Выберете час ⌚, когда будет срабатывать напоминание',
-    'pick_minutes': 'Выберете минуту ⌚, когда будет срабатывать напоминание'
+    'pick_minutes': 'Выберете минуту ⌚, когда будет срабатывать напоминание',
+    'choice_days': 'Выберите дни недели, в которые должно срабатывать уведомление',
+    'choice_year': 'Выберите год',
+    'month_choice': 'Выберите месяц',
+    'day_choice': 'Выберите день',
 }
 
 LEXICON_BUTTONS: dict = {
@@ -19,14 +23,40 @@ LEXICON_BUTTONS: dict = {
     '10_minutes_add': '⌚ Через 10 минут',
     '30_minutes_add': '⌚ Через 30 минут',
     'hour_add': '⌚ Через час',
-    'day_add': '⌚ Завтра',
     'custom_add': '⌚ Другое время',
     'backward': '<',
-    'forward': '>'
+    'forward': '>',
+    'week_days': 'Дни недели',
+    'exact_date': 'Точна дата',
+    'confirm': '✅ Подтвердить'
 }
 
 LEXICON_MENU_COMMANDS = {
     '/start': 'Начать',
     '/add_notification': 'Как добавить напоминание',
     '/remove_notification': 'Как удалить напоминание',
+}
+
+day_name_ru = {
+    'Monday': 'Пн',
+    'Tuesday': 'Вт',
+    'Wednesday': 'Ср',
+    'Thursday': 'Чт',
+    'Friday': 'Пт',
+    'Saturday': 'Сб',
+    'Sunday': 'Вс'
+}
+month_name_ru = {
+    'January': 'Янв',
+    'February': 'Фев',
+    'March': 'Мар',
+    'April': 'Апр',
+    'May': 'Май',
+    'June': 'Июн',
+    'July': 'Июл',
+    'August': 'Авг',
+    'September': 'Сен',
+    'October': 'Окт',
+    'November': 'Ноя',
+    'December': 'Дек'
 }
