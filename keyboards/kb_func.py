32dc0126fdbbaf338874bfb98c203dceae169921
Mabarik667f <111702128+Mabarik667f@users.noreply.github.com>
2023-11-16 22:38:45 +0300

    create hours and minutes keyboards, first iteration

--- keyboards/kb_func.py
+++ keyboards/kb_func.py
@@ -1,5 +1,5 @@
 import calendar
-import datetime
+from datetime import datetime
 
 from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 from aiogram.utils.keyboard import InlineKeyboardBuilder
@@ -90,8 +90,12 @@ obj_calendar = calendar.Calendar(firstweekday=0)
 
 
 def get_days(year, month):
-    days_month = obj_calendar.itermonthdates(year, month)
+    days_month = obj_calendar.itermonthdays(year, month)
     return days_month
+
+
+def get_hours(year, month, day):
+    curr_hour = datetime.now().hour
 
 
 def add_week_kb(kb):
@@ -102,13 +106,21 @@ def add_week_kb(kb):
 
 
 class DialogCalendar:
-    @staticmethod
-    async def year_calendar():
-        current_year = datetime.datetime.now().year
+
+    def __init__(self):
+        self.curr_year = datetime.now().year
+        self.curr_month = datetime.now().month
+        self.curr_day = datetime.now().day
+        self.curr_hour = datetime.now().hour
+        self.curr_minute = datetime.now().minute
+        self.base_button = InlineKeyboardButton(text=' ',
+                                                callback_data='ignore_callback')
+
+    async def year_calendar(self):
         year_buttons: list[InlineKeyboardButton] = [
             InlineKeyboardButton(
-                text=int(current_year) + year,
-                callback_data=CalendarFactory(year=int(current_year) + year, month=0, day=0).pack())
+                text=int(self.curr_year) + year,
+                callback_data=CalendarFactory(year=int(self.curr_year) + year, month=0, day=0).pack())
             for year in range(0, 4)]
 
         kb_builder = InlineKeyboardBuilder()
@@ -116,11 +128,15 @@ class DialogCalendar:
         add_base_buttons(kb_builder)
         return kb_builder.as_markup()
 
-    @staticmethod
-    async def get_month(year):
+    async def get_month(self, year):
         month_buttons: list[
             InlineKeyboardButton] = []
-        for month in range(1, 13):
+
+        start_month = 1
+        if year == self.curr_year:
+            start_month = self.curr_month
+
+        for month in range(start_month, 13):
             month_ru, month_en = get_month_name(month)
             month_button = InlineKeyboardButton(
                 text=month_ru,
@@ -133,25 +149,89 @@ class DialogCalendar:
         add_base_buttons(month_builder)
         return month_builder.as_markup()
 
-    @staticmethod
-    async def get_day(year, month):
+    async def get_day(self, year, month):
         choice_day: list[InlineKeyboardButton] = []
         days = get_days(year, month)
+
         for day in days:
-            d = day.day
-            if day.month == month:
-                text = str(d)
-                callback_data = CalendarFactory(year=year, month=month, day=d).pack()
+            if month == self.curr_month and day < self.curr_day and year == self.curr_year:
+                day_button = self.base_button
+            elif day != 0:
+                day_button = InlineKeyboardButton(
+                    text=day,
+                    callback_data=CalendarFactory(year=year, month=month, day=day).pack()
+                )
             else:
-                text = ' '
-                callback_data = 'ignore_callback'
-            day_button = InlineKeyboardButton(text=text,
-                                              callback_data=callback_data)
+                day_button = self.base_button
+
+            day_button = day_button
             choice_day.append(day_button)
 
         day_builder = InlineKeyboardBuilder()
         date_header_buttons(day_builder, year=year, month=month)
         add_week_kb(day_builder)
-        day_builder.row(*choice_day, width=7)
+        for i in range(0, len(choice_day), 7):
+            if all(i == self.base_button for i in choice_day[i:i + 7]):
+                continue
+            else:
+                day_builder.row(*choice_day[i:i + 7], width=7)
         add_base_buttons(day_builder)
         return day_builder.as_markup()
+
+    async def get_hour(self, year, month, day):
+        choice_hours: list[InlineKeyboardButton] = []
+        beg = 0
+        if day == self.curr_day and month == self.curr_month and self.curr_year == year:
+            beg = self.curr_hour
+            while beg % 4 != 0:
+                beg -= 1
+        for hour in range(beg, 24):
+            if day == self.curr_day and month == self.curr_month and self.curr_year == year and self.curr_hour > hour:
+                hour_button = self.base_button
+            else:
+                hour_button = InlineKeyboardButton(
+                    text=hour,
+                    callback_data=CalendarFactory(year=year, month=month, day=day, hour=hour).pack())
+
+            choice_hours.append(hour_button)
+
+        hour_builder = InlineKeyboardBuilder()
+        for i in range(0, len(choice_hours), 4):
+            if all(i == self.base_button for i in choice_hours[i:i + 4]):
+                continue
+            else:
+                hour_builder.row(*choice_hours[i:i + 4], width=4)
+        add_base_buttons(hour_builder)
+        return hour_builder.as_markup()
+
+    async def get_minutes(self, year, month, day, hour):
+        choice_minutes: list[InlineKeyboardButton] = []
+        beg = 0
+        if day == self.curr_day and month == self.curr_month and self.curr_year == year \
+                and self.curr_hour == hour:
+            beg = self.curr_minute
+            while beg % 5 != 0:
+                beg -= 1
+        for minute in range(beg, 60):
+            if day == self.curr_day and month == self.curr_month and self.curr_year == year \
+                    and self.curr_hour == hour and self.curr_minute > minute:
+                minute_button = self.base_button
+            elif minute == self.curr_minute and day == self.curr_day and month == self.curr_month and self.curr_year == year \
+                    and self.curr_hour == hour:
+                minute_button = self.base_button
+            else:
+                minute_button = InlineKeyboardButton(
+                    text=minute,
+                    callback_data=CalendarFactory(year=year, month=month, day=day,
+                                                  hour=hour, minute=minute).pack())
+
+            choice_minutes.append(minute_button)
+
+        minute_builder = InlineKeyboardBuilder()
+        for i in range(0, len(choice_minutes), 5):
+            if all(i == self.base_button for i in choice_minutes[i:i + 5]):
+                continue
+            else:
+                minute_builder.row(*choice_minutes[i:i + 5], width=5)
+        add_base_buttons(minute_builder)
+        return minute_builder.as_markup()
